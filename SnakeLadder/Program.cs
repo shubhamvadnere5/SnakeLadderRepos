123ec224636e457b057e815448a10e02fd6a478d
ShubhamPV <shubhamvadnere5@gmail.com>
2021-09-16 09:09:17 +0530

    [ShubhamPV] Player at start position

---
+++ SnakeLadder/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeLadder
+{
+    class SnakeLadder
+    {
+        public static void Position()
+        {
+            int position = 0;
+            Console.WriteLine("Player is at start position:" + position);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Single player");
+            SnakeLadder.Position();
+            Console.ReadLine();
+        }
+    }
+}
