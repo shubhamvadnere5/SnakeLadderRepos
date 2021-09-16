be84981d3394b4aa308db4c4258f9f8e468cc3aa
ShubhamPV <shubhamvadnere5@gmail.com>
2021-09-16 09:32:09 +0530

    [ShubhamPV] Adding UC3

--- SnakeLadder/Program.cs
+++ SnakeLadder/Program.cs
@@ -6,19 +6,50 @@ using System.Threading.Tasks;
 
 namespace SnakeLadder
 {
+
+
     class SnakeLadder
     {
-        //UC2 Rolls a Dice
-        public static void CheckDice()
+        /// <summary>
+        /// UC3
+        /// </summary>
+        /// <returns></returns>
+
+        public static int CheckDice()
         {
             Random random = new Random();
             int CheckDice = random.Next(1, 6);
-            Console.WriteLine("The player rolls a dice:" + CheckDice);
+            Console.WriteLine("Dice number:" + CheckDice);
+            return CheckDice;
+        }
+        public static void CheckOption()
+        {
+            Random random = new Random();
+            int CheckOption = random.Next(0, 3);
+            int position = 0;
+            int dice = SnakeLadder.CheckDice();
+            //switch selection statement
+            switch (CheckOption)
+            {
+                case 0:
+                    Console.WriteLine("No play");
+                    break;
+                case 1:
+                    Console.WriteLine("Snake");
+                    position -= dice;
+                    Console.WriteLine("Player current position" + position);
+                    break;
+                case 2:
+                    Console.WriteLine("Ladder");
+                    position += dice;
+                    Console.WriteLine("Player current position" + position);
+                    break;
+            }
         }
 
         static void Main(string[] args)
         {
-            SnakeLadder.CheckDice();
+            SnakeLadder.CheckOption();
             Console.ReadLine();
         }
     }
